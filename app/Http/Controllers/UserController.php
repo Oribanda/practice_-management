5dd1df1569452c2e569a759a0c25dffa2c414d4a
oribanda <oribanda2@gmail.com>
2020-09-25 08:26:47 +0900

    avatar validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -95,22 +95,24 @@ class UserController extends Controller
             'email'                   => 'required|email',
             'password'                => 'required|confirmed|min:8|max:8|confirmed',
             'password_confirmation'   => 'required',
-            'avatar'                  => 'nullable|file|image|max:10000',
+            'avatar'                  => 'nullable|file|mimes:jpeg,png,jpg|max:10000',
             'introduce'               => 'nullable|string|max:300',
         ];
 
         $messages = [
-            'name.required'     => '名前を入力して下さい。',
-            'name.max'          => '名前は:max文字以内で入力して下さい。',
-            'email.required'    => 'メールアドレスを入力して下さい。',
-            'email.email'       => '正しいメールアドレスを入力して下さい。',
-            'password.required' => 'パスワードを入力して下さい。',
-            'password.min'      => 'パスワードは:min文字以上で入力して下さい。',
-            'password.max'      => 'パスワードは:max文字以内で入力して下さい。',
-            'confirmed'         => ':attributeと、パスワードが一致していません。',
-            'avatar.image'      => 'アバターは:imageを選択して下さい。',
-            'avatar.max'        => 'アバターは:max以下の画像ファイルを選択して下さい。',
-            'introduce.max'     => '文章は:max文字以内で入力して下さい。',
+            'name.required'           => '名前を入力して下さい。',
+            'name.max'                => '名前は:max文字以内で入力して下さい。',
+            'email.required'          => 'メールアドレスを入力して下さい。',
+            'email.email'             => '正しいメールアドレスを入力して下さい。',
+            'password.required'       => 'パスワードを入力して下さい。',
+            'password.min'            => 'パスワードは:min文字以上で入力して下さい。',
+            'password.max'            => 'パスワードは:max文字以内で入力して下さい。',
+            'confirmed'               => 'パスワードと確認用パスワードが一致していません。',
+            'required'                => '確認用パスワードを入力して下さい。',
+            'avatar.image'            => '画像はjpeg,png,jpgのいずれかの画像を選択して下さい。',
+            'avatar.mimes'            => '画像はjpeg,png,jpgのいずれかの画像を選択して下さい。',
+            'avatar.max'              => '画像は:max以下の画像ファイルを選択して下さい。',
+            'introduce.max'           => '文章は:max文字以内で入力して下さい。',
         ];
 
         $validator = validator($request->except(['avatar']), $rules, $messages);
